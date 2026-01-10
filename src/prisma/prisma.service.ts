16bff39a1f69e3e8910cada50e7a1afc87c13713
shariyerShazan <shariyershazan1@gmail.com>
2026-01-11 02:53:38 +0600

    admin:any declared

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -8,7 +8,6 @@ export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
-  admin: PrismaClient['admin'];
   constructor() {
     const adapter = new PrismaPg({
       connectionString: process.env.DATABASE_URL,
