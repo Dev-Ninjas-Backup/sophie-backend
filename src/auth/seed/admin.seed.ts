9649b1126daad397d023cf89a6c4357aff2c571a
shariyerShazan <shariyershazan1@gmail.com>
2026-01-11 01:43:57 +0600

    minor chnages

--- src/auth/seed/admin.seed.ts
+++ src/auth/seed/admin.seed.ts
@@ -1,14 +1,11 @@
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-import { PrismaClient } from '@prisma/client';
-
+import { PrismaService } from '@/prisma/prisma.service';
 import { Injectable, OnModuleInit } from '@nestjs/common';
-
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class SeedService implements OnModuleInit {
-  private prisma = new PrismaClient();
+  constructor(private prisma: PrismaService) {}
+
   async onModuleInit() {
     await this.seedAdmin();
   }
@@ -16,27 +13,20 @@ export class SeedService implements OnModuleInit {
   async seedAdmin() {
     const adminEmail = 'talharris14@gmail.com';
 
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     const existingAdmin = await this.prisma.admin.findUnique({
       where: { email: adminEmail },
     });
 
-    if (existingAdmin) {
-      console.log('Admin already exists. Skipping seed.');
-      return;
+    if (!existingAdmin) {
+      const hashedPassword = await bcrypt.hash('Admin123', 12);
+      await this.prisma.admin.create({
+        data: {
+          email: adminEmail,
+          password: hashedPassword,
+          role: 'ADMIN',
+        },
+      });
+      console.log('Admin seeded!');
     }
-
-    const hashedPassword = await bcrypt.hash('Admin123', 12);
-
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-    await this.prisma.admin.create({
-      data: {
-        email: adminEmail,
-        password: hashedPassword,
-        role: 'ADMIN',
-      },
-    });
-
-    console.log('🚀 Admin seeded successfully!');
   }
 }
